0c83433fc1890e10c280d0c166e1a1f017ecac59
miaojuanfeng <miaojuanfeng@qq.com>
2015-09-02 04:54:31 -0700

    update CII_Output::display()

--- cii_output.c
+++ cii_output.c
@@ -30,9 +30,7 @@ ZEND_API void cii_append_output(zend_class_entry *cii_output_ce, zval *cii_outpu
 	zval *new_final_output;
 	MAKE_STD_ZVAL(new_final_output);
 	ZVAL_STRING(new_final_output, output_new, 0);
-	//php_printf("Z_STRVAL_P: %s\n", Z_STRVAL_P(new_final_output));
-	//php_printf("Z_STRLEN_P: %d\n", Z_STRLEN_P(new_final_output));
-	//php_printf("output_new: %s\n", output_new);
+
 	zend_update_property(cii_output_ce, cii_output_obj, "final_output", 12, new_final_output TSRMLS_CC);
 
 	zval_ptr_dtor(&new_final_output);
@@ -61,10 +59,105 @@ PHP_METHOD(cii_output, append_output)
 		RETURN_ZVAL(getThis(), 1, 0);
 	}
 }
+/*
+*	display
+*/
+ZEND_API int cii_display(char *output, uint output_len, char **output_new, uint *output_new_len TSRMLS_DC){
+	char *p = NULL;
+	char *free_output_new = NULL;
+	char retval = 0;
+	*output_new = output;
+	*output_new_len = output_len;
+	/*
+	*	replace {elapsed_time}
+	*/
+	do{
+		if( p = strstr(*output_new, "{elapsed_time}") ){
+			p[13] = '\0';
+			p[0]  = '\0';
+			if( retval ){
+				free_output_new = *output_new;
+			}
+			*output_new_len = spprintf(output_new, 0, "%s%s%s", *output_new, "0.0012", &p[14]);
+			if( free_output_new ){
+				efree(free_output_new);
+				free_output_new = NULL;
+			}
+			retval = 1;
+		}
+	}while(p);
+	/*
+	*	replace {memory_usage}
+	*/
+	do{
+		if( p = strstr(*output_new, "{memory_usage}") ){
+			p[13] = '\0';
+			p[0]  = '\0';
+			if( retval ){
+				free_output_new = *output_new;
+			}
+			char *memory = _php_math_number_format((double)zend_memory_usage(0 TSRMLS_DC)/1024/1024, 2, '.', ',');
+			*output_new_len = spprintf(output_new, 0, "%s%s%s%s", *output_new, memory, "MB", &p[14]);
+			efree(memory);
+			if( free_output_new ){
+				efree(free_output_new);
+				free_output_new = NULL;
+			}
+			retval = 1;
+		}
+	}while(p);
+	/*
+	*	return state. 1 means replaced or 0 means not replaced
+	*/
+	return retval;
+}
+/**
+* Display Output
+*
+* Processes and sends finalized output data to the browser along
+* with any server headers and profile data. It also stops benchmark
+* timers so the page rendering speed and memory usage can be shown.
+*
+* Note: All "view" data is automatically put into $this->final_output
+*	 by controller class.
+*
+* @uses	CI_Output::$final_output
+* @param	string	$output	Output data override
+* @return	void
+*
+* public function display($output = '')
+*/
+PHP_METHOD(cii_output, display)
+{
+	zval *output = NULL;
+	char *output_new;
+	uint output_new_len;
+	char retval;
+
+	if (zend_parse_parameters(ZEND_NUM_ARGS() TSRMLS_CC, "|z!" ,&output) == FAILURE) {
+		WRONG_PARAM_COUNT;
+	}
+
+	if( output && Z_TYPE_P(output) != IS_STRING ){
+		convert_to_string(output);
+	}
+
+	if( output && Z_TYPE_P(output) == IS_STRING ){
+		retval = cii_display(Z_STRVAL_P(output), Z_STRLEN_P(output), &output_new, &output_new_len TSRMLS_CC);
+	}else{
+		zval *output = zend_read_property(cii_output_ce, getThis(), "final_output", 12, 1 TSRMLS_CC);
+		retval = cii_display(Z_STRVAL_P(output), Z_STRLEN_P(output), &output_new, &output_new_len TSRMLS_CC);
+	}
+	PHPWRITE(output_new, output_new_len);
+	if( retval ){
+		efree(output_new);
+	}
+}
 
 zend_function_entry cii_output_methods[] = {
 	PHP_ME(cii_output, __construct, NULL, ZEND_ACC_PUBLIC | ZEND_ACC_CTOR)
 	PHP_ME(cii_output, append_output, NULL, ZEND_ACC_PUBLIC)
+	PHP_ME(cii_output, display, NULL, ZEND_ACC_PUBLIC)
 	PHP_FE_END
 };
 
--- cii_router.c
+++ cii_router.c
@@ -41,7 +41,7 @@ PHP_METHOD(cii_router, __construct)
 	if( zend_hash_index_find(Z_ARRVAL_P(rsegments), 1, (void**)&class) == FAILURE ||
 		Z_TYPE_PP(class) == IS_STRING && Z_STRLEN_PP(class) == 0 ){
 		MAKE_STD_ZVAL(rsegments_class);
-		ZVAL_STRING(rsegments_class, "home", 1);
+		ZVAL_STRING(rsegments_class, "welcome", 1);
 		zend_hash_index_update(Z_ARRVAL_P(rsegments), 1, &rsegments_class, sizeof(zval *), NULL);
 	}else{
 		rsegments_class = *class;
